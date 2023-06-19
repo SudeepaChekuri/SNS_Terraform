2f0a88ccb9428836154a7428fb2b46532ea31039
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-06-19 16:46:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,63 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "ap-south-1"  # Replace with your desired region
+}
+
+# Create an SNS topic
+resource "aws_sns_topic" "my_topic" {
+  name = "my-topic"  # Replace with your desired topic name
+}
+
+# Create an SQS queue
+resource "aws_sqs_queue" "my_queue" {
+  name = "my-queue"  # Replace with your desired queue name
+}
+
+# Configure SNS to publish to SQS
+resource "aws_sns_topic_subscription" "sqs_subscription" {
+  topic_arn = aws_sns_topic.my_topic.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.my_queue.arn
+}
+
+# Create an IAM role for the Lambda function
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# Attach an IAM policy to the Lambda role
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
+
+# Create the Lambda function
+resource "aws_lambda_function" "my_lambda" {
+  function_name    = "my-lambda"  # Replace with your desired function name
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.handler"  # Replace with your Lambda function handler
+  runtime          = "nodejs14.x"  # Replace with your desired runtime
+  timeout          = 10  # Replace with your desired timeout value
+  memory_size      = 128  # Replace with your desired memory size
+  publish          = true  # Set to false if you don't want to publish a new version
+  source_code_hash = filebase64sha256("lambda_function.zip")  # Replace with your Lambda deployment package filename
+
+  # Specify your Lambda function's source code
+  filename = "lambda_function.zip"  # Replace with your Lambda deployment package filename
+}
